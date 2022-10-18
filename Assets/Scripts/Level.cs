6f4d30e07b4cfe369002509d049f194af3dfe798
KamilMaliszewskiGameDev <kmaliszewskicontact@gmail.com>
2022-10-18 16:35:48 +0200

    created class for pipe and destroying pipes when offscreen

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,11 +7,36 @@ public class Level : MonoBehaviour
     private const float PIPE_WIDTH = 7.8f;
     private const float PIPE_HEAD_HEIGHT = 3.85f;
     private const float CAMERA_ORTHO_SIZE = 50f;
+    private const float PIPE_MOVE_SPEED = 30f;
+    private const float PIPE_DESTROY_X_POSITION = -150f;
+
+    private List<Pipe> pipeList;
+
+    private void Awake(){
+        pipeList = new List<Pipe>();
+    }
 
     private void Start(){
         CreateGapPipes(50f, 20f, 20f);
     }
 
+    private void Update(){
+        HandlePipeMovement();
+    }
+    
+
+    private void HandlePipeMovement(){
+        for(int i=0; i<pipeList.Count; i++){
+            Pipe pipe = pipeList[i];
+            pipe.Move();
+            if(pipe.GetXPosition() < PIPE_DESTROY_X_POSITION){
+                pipe.DestroySelf();
+                pipeList.Remove(pipe);
+                i--;
+            }
+
+        }
+    }
 
     private void CreateGapPipes(float gapY, float gapSize, float posX){
         CreatePipe(gapY - gapSize * 0.5f, posX, true);
@@ -41,13 +66,40 @@ public class Level : MonoBehaviour
             pipeBodyPosY = +CAMERA_ORTHO_SIZE;
             pipeBody.localScale = new Vector3(1, -1, 1);
         }
-
         pipeBody.position = new Vector3(posX, pipeBodyPosY);
+
         SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySpriteRenderer.size = new Vector2(PIPE_WIDTH, height);
 
         BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
         pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * 0.5f);
+
+        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        pipeList.Add(pipe);
+    }
+
+    private class Pipe {
+        private Transform pipeHeadTransform;
+        private Transform pipeBodyTransform;
+
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform){
+            this.pipeBodyTransform = pipeBodyTransform;
+            this.pipeHeadTransform = pipeHeadTransform;
+        }
+
+        public void Move(){
+            pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+
+        public float GetXPosition(){
+            return pipeHeadTransform.position.x;
+        }
+
+        public void DestroySelf(){
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
+        }
     }
 }
