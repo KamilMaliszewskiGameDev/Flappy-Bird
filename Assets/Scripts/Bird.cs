e4f6c77a96e802e8a24dbdacef8d85c6bec99a8d
KamilMaliszewskiGameDev <kmaliszewskicontact@gmail.com>
2022-10-18 13:21:51 +0200

    added jump to bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    private const float JUMP_AMOUNT = 100f;
+    private Rigidbody2D rigidBody2D;
+
+    private void Awake(){
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
     private void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }
 
     }
     
     private void Jump(){
-        
+        rigidBody2D.velocity = Vector2.up * JUMP_AMOUNT;
     }
 }
