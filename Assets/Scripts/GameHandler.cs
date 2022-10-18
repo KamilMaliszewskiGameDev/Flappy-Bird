0e76d2c4d8a3b2f69eb22b4875934d1029f99526
KamilMaliszewskiGameDev <kmaliszewskicontact@gmail.com>
2022-10-18 12:58:51 +0200

    added game assets into the game and view is setup

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,8 +7,8 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameHandler.Start");
-
+        GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
+        gameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.GetInstance().pipeHeadSprite;
     }
 
 
